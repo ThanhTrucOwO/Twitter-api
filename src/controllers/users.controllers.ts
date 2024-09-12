a52678c098901501ca44a4750f67e7fb82a77fd3
ThanhTrucOwO <kusodanma14@gmail.com>
2024-09-12 11:54:10 +0700

    create access token & refresh token

---
+++ src/controllers/users.controllers.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from 'express'
+import { ParamsDictionary } from 'express-serve-static-core'
+import { RegisterReqBody } from '~/models/requests/User.requests'
+import User from '~/models/schemas/User.schema'
+import databaseService from '~/services/database.services'
+import usersService from '~/services/users.services'
+
+export const loginController = (req: Request, res: Response) => {
+  const { email, password } = req.body
+  if (email === 'kusodanma14@gmail.com' && password === '123456') {
+    return res.json({
+      message: 'Login success'
+    })
+  }
+  return res.status(400).json({
+    error: 'Login failed'
+  })
+}
+
+export const registerController = async (req: Request<ParamsDictionary, any, RegisterReqBody>, res: Response) => {
+  try {
+    const result = await usersService.register(req.body)
+    console.log(result)
+    // await databaseService.users.find({})
+    return res.json({
+      message: 'Register success',
+      result
+    })
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json({
+      message: 'Register failed',
+      error
+    })
+  }
+}
