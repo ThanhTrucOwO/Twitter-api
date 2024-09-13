4f7403dcb42861e63c170e59b9ee8454c2a94fdb
ThanhTrucOwO <kusodanma14@gmail.com>
2024-09-13 15:35:36 +0700

    Handle access token for logout

--- src/middlewares/users.middlewares.ts
+++ src/middlewares/users.middlewares.ts
@@ -1,158 +1,198 @@
 import { checkSchema } from 'express-validator'
+import HTTP_STATUS from '~/constants/httpStatus'
 import { USERS_MESSAGE } from '~/constants/messages'
+import { ErrorWithStatus } from '~/models/Errors'
 import databaseService from '~/services/database.services'
 import usersService from '~/services/users.services'
 import { hashPassword } from '~/utils/crypto'
+import { verifyToken } from '~/utils/jwt'
 import { validate } from '~/utils/validation'
 
 export const loginValidator = validate(
-  checkSchema({
-    email: {
-      isEmail: {
-        errorMessage: USERS_MESSAGE.EMAIL_IS_INVALID
-      },
-      trim: true,
-      custom: {
-        options: async (value, { req }) => {
-          const user = await databaseService.users.findOne({ email: value, password: hashPassword(req.body.password) })
-          if (user === null) {
-            throw new Error(USERS_MESSAGE.EMAIL_OR_PASSWORD_IS_INCORRECT)
+  checkSchema(
+    {
+      email: {
+        isEmail: {
+          errorMessage: USERS_MESSAGE.EMAIL_IS_INVALID
+        },
+        trim: true,
+        custom: {
+          options: async (value, { req }) => {
+            const user = await databaseService.users.findOne({
+              email: value,
+              password: hashPassword(req.body.password)
+            })
+            if (user === null) {
+              throw new Error(USERS_MESSAGE.EMAIL_OR_PASSWORD_IS_INCORRECT)
+            }
+            req.user = user
+            return true
           }
-          req.user = user
-          return true
+        }
+      },
+      password: {
+        notEmpty: {
+          errorMessage: USERS_MESSAGE.PASSWORD_IS_REQUIRED
+        },
+        isString: {
+          errorMessage: USERS_MESSAGE.CONFIRM_PASSWORD_MUST_BE_A_STRING
+        },
+        isLength: {
+          options: {
+            min: 6,
+            max: 50
+          },
+          errorMessage: USERS_MESSAGE.PASSWORD_LENGTH_MUST_BE_FROM_6_TO_50
+        },
+        isStrongPassword: {
+          options: {
+            minLength: 6,
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 1
+          },
+          errorMessage: USERS_MESSAGE.PASSWORD_MUST_BE_STRONG
         }
       }
     },
-    password: {
-      notEmpty: {
-        errorMessage: USERS_MESSAGE.PASSWORD_IS_REQUIRED
-      },
-      isString: {
-        errorMessage: USERS_MESSAGE.CONFIRM_PASSWORD_MUST_BE_A_STRING
-      },
-      isLength: {
-        options: {
-          min: 6,
-          max: 50
-        },
-        errorMessage: USERS_MESSAGE.PASSWORD_LENGTH_MUST_BE_FROM_6_TO_50
-      },
-      isStrongPassword: {
-        options: {
-          minLength: 6,
-          minLowercase: 1,
-          minUppercase: 1,
-          minNumbers: 1,
-          minSymbols: 1
-        },
-        errorMessage: USERS_MESSAGE.PASSWORD_MUST_BE_STRONG
-      }
-    }
-  })
+    ['body']
+  )
 )
 
 export const registerValidator = validate(
-  checkSchema({
-    name: {
-      notEmpty: {
-        errorMessage: USERS_MESSAGE.NAME_IS_REQUIRED
-      },
-      isString: {
-        errorMessage: USERS_MESSAGE.NAME_MUST_BE_A_STRING
-      },
-      isLength: {
-        options: {
-          min: 1,
-          max: 100
+  checkSchema(
+    {
+      name: {
+        notEmpty: {
+          errorMessage: USERS_MESSAGE.NAME_IS_REQUIRED
         },
-        errorMessage: USERS_MESSAGE.NAME_LENGTH_MUST_BE_FROM_1_TO_100
+        isString: {
+          errorMessage: USERS_MESSAGE.NAME_MUST_BE_A_STRING
+        },
+        isLength: {
+          options: {
+            min: 1,
+            max: 100
+          },
+          errorMessage: USERS_MESSAGE.NAME_LENGTH_MUST_BE_FROM_1_TO_100
+        },
+        trim: true
       },
-      trim: true
-    },
-    email: {
-      notEmpty: {
-        errorMessage: USERS_MESSAGE.EMAIL_IS_REQUIRED
-      },
-      isEmail: {
-        errorMessage: USERS_MESSAGE.EMAIL_IS_INVALID
-      },
-      trim: true,
-      custom: {
-        options: async (value) => {
-          const isExistEmail = await usersService.checkEmailExist(value)
-          if (isExistEmail) {
-            throw new Error(USERS_MESSAGE.EMAIL_ALREADY_EXISTS)
+      email: {
+        notEmpty: {
+          errorMessage: USERS_MESSAGE.EMAIL_IS_REQUIRED
+        },
+        isEmail: {
+          errorMessage: USERS_MESSAGE.EMAIL_IS_INVALID
+        },
+        trim: true,
+        custom: {
+          options: async (value) => {
+            const isExistEmail = await usersService.checkEmailExist(value)
+            if (isExistEmail) {
+              throw new Error(USERS_MESSAGE.EMAIL_ALREADY_EXISTS)
+            }
+            return true
           }
-          return true
+        }
+      },
+      password: {
+        notEmpty: {
+          errorMessage: USERS_MESSAGE.PASSWORD_IS_REQUIRED
+        },
+        isString: {
+          errorMessage: USERS_MESSAGE.CONFIRM_PASSWORD_MUST_BE_A_STRING
+        },
+        isLength: {
+          options: {
+            min: 6,
+            max: 50
+          },
+          errorMessage: USERS_MESSAGE.PASSWORD_LENGTH_MUST_BE_FROM_6_TO_50
+        },
+        isStrongPassword: {
+          options: {
+            minLength: 6,
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 1
+          },
+          errorMessage: USERS_MESSAGE.PASSWORD_MUST_BE_STRONG
+        }
+      },
+      confirm_password: {
+        notEmpty: {
+          errorMessage: USERS_MESSAGE.CONFIRM_PASSWORD_IS_REQUIRED
+        },
+        isString: {
+          errorMessage: USERS_MESSAGE.CONFIRM_PASSWORD_MUST_BE_A_STRING
+        },
+        isLength: {
+          options: {
+            min: 6,
+            max: 50
+          },
+          errorMessage: USERS_MESSAGE.CONFIRM_PASSWORD_LENGTH_MUST_BE_FROM_6_TO_50
+        },
+        isStrongPassword: {
+          options: {
+            minLength: 6,
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 1
+          },
+          errorMessage: USERS_MESSAGE.CONFIRM_PASSWORD_MUST_BE_STRONG
+        },
+        custom: {
+          options: (value, { req }) => {
+            if (value !== req.body.password) {
+              throw new Error(USERS_MESSAGE.CONFRIM_PASSWORD_MUST_BE_THE_SAME_AS_PASSWORD)
+            }
+            return true
+          }
+        }
+      },
+      date_of_birth: {
+        isISO8601: {
+          options: {
+            strict: true,
+            strictSeparator: true
+          },
+          errorMessage: USERS_MESSAGE.DATE_OF_BIRTH_MUST_BE_ISO8601
         }
       }
     },
-    password: {
-      notEmpty: {
-        errorMessage: USERS_MESSAGE.PASSWORD_IS_REQUIRED
-      },
-      isString: {
-        errorMessage: USERS_MESSAGE.CONFIRM_PASSWORD_MUST_BE_A_STRING
-      },
-      isLength: {
-        options: {
-          min: 6,
-          max: 50
+    ['body']
+  )
+)
+
+export const accessTokenValidator = validate(
+  checkSchema(
+    {
+      Authorization: {
+        notEmpty: {
+          errorMessage: USERS_MESSAGE.ACCESS_TOKEN_IS_REQUIRED
         },
-        errorMessage: USERS_MESSAGE.PASSWORD_LENGTH_MUST_BE_FROM_6_TO_50
-      },
-      isStrongPassword: {
-        options: {
-          minLength: 6,
-          minLowercase: 1,
-          minUppercase: 1,
-          minNumbers: 1,
-          minSymbols: 1
-        },
-        errorMessage: USERS_MESSAGE.PASSWORD_MUST_BE_STRONG
-      }
-    },
-    confirm_password: {
-      notEmpty: {
-        errorMessage: USERS_MESSAGE.CONFIRM_PASSWORD_IS_REQUIRED
-      },
-      isString: {
-        errorMessage: USERS_MESSAGE.CONFIRM_PASSWORD_MUST_BE_A_STRING
-      },
-      isLength: {
-        options: {
-          min: 6,
-          max: 50
-        },
-        errorMessage: USERS_MESSAGE.CONFIRM_PASSWORD_LENGTH_MUST_BE_FROM_6_TO_50
-      },
-      isStrongPassword: {
-        options: {
-          minLength: 6,
-          minLowercase: 1,
-          minUppercase: 1,
-          minNumbers: 1,
-          minSymbols: 1
-        },
-        errorMessage: USERS_MESSAGE.CONFIRM_PASSWORD_MUST_BE_STRONG
-      },
-      custom: {
-        options: (value, { req }) => {
-          if (value !== req.body.password) {
-            throw new Error(USERS_MESSAGE.CONFRIM_PASSWORD_MUST_BE_THE_SAME_AS_PASSWORD)
+        custom: {
+          options: async (value: string, { req }) => {
+            const access_token = value.split(' ')[1]
+            console.log(access_token)
+            if (!access_token) {
+              throw new ErrorWithStatus({
+                message: USERS_MESSAGE.ACCESS_TOKEN_IS_REQUIRED,
+                status: HTTP_STATUS.UNAUTHORIZED
+              })
+            }
+            const decoded_authorization = await verifyToken({ token: access_token })
+            req.decoded_authorization = decoded_authorization
+            return true
           }
-          return true
         }
       }
     },
-    date_of_birth: {
-      isISO8601: {
-        options: {
-          strict: true,
-          strictSeparator: true
-        },
-        errorMessage: USERS_MESSAGE.DATE_OF_BIRTH_MUST_BE_ISO8601
-      }
-    }
-  })
+    ['headers']
+  )
 )
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,15 @@
 import { Router } from 'express'
 import { loginController, registerController } from '~/controllers/users.controllers'
-import { loginValidator, registerValidator } from '~/middlewares/users.middlewares'
+import { accessTokenValidator, loginValidator, registerValidator } from '~/middlewares/users.middlewares'
 import { wrapRequestHandler } from '~/utils/handlers'
 const usersRouter = Router()
+
+/**
+ * Description: Login a user
+ * Path: /login
+ * Method: POST
+ * Body: {email: string, password: string}
+ */
 
 usersRouter.post('/login', loginValidator, wrapRequestHandler(loginController))
 /**
@@ -13,5 +20,21 @@ usersRouter.post('/login', loginValidator, wrapRequestHandler(loginController))
  */
 
 usersRouter.post('/register', registerValidator, wrapRequestHandler(registerController))
+
+/**
+ * Description: Logout a user
+ * Path: /logout
+ * Method: POST
+ * Headers: {Authorization: Bearer <access_token>}
+ * Body: { refresh_token: string }
+ */
+
+usersRouter.post(
+  '/logout',
+  accessTokenValidator,
+  wrapRequestHandler((req, res) => {
+    res.json({ message: 'Logout success' })
+  })
+)
 
 export default usersRouter
