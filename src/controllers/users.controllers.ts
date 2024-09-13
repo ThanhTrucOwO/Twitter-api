9ab43e51ec9de332200310841a234eff7a5c45ca
ThanhTrucOwO <kusodanma14@gmail.com>
2024-09-13 14:38:52 +0700

    Handle login and update config env

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,19 +1,19 @@
 import { NextFunction, Request, Response } from 'express'
 import { ParamsDictionary } from 'express-serve-static-core'
+import { ObjectId } from 'mongodb'
+import { USERS_MESSAGE } from '~/constants/messages'
 import { RegisterReqBody } from '~/models/requests/User.requests'
 import User from '~/models/schemas/User.schema'
 import databaseService from '~/services/database.services'
 import usersService from '~/services/users.services'
 
-export const loginController = (req: Request, res: Response) => {
-  const { email, password } = req.body
-  if (email === 'user123@gmail.com' && password === '123456') {
-    return res.json({
-      message: 'Login success'
-    })
-  }
-  return res.status(400).json({
-    error: 'Login failed'
+export const loginController = async (req: Request, res: Response) => {
+  const user = req.user as User
+  const user_id = user._id as ObjectId
+  const result = await usersService.login(user_id.toString())
+  return res.json({
+    message: USERS_MESSAGE.LOGIN_SUCCESS,
+    result
   })
 }
 
@@ -22,11 +22,10 @@ export const registerController = async (
   res: Response,
   next: NextFunction
 ) => {
-  throw new Error('Test error')
   const result = await usersService.register(req.body)
   // await databaseService.users.find({})
   return res.json({
-    message: 'Register success',
+    message: USERS_MESSAGE.REGISTER_SUCCESS,
     result
   })
 }
--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -4,7 +4,10 @@ import { RegisterReqBody } from '~/models/requests/User.requests'
 import { hashPassword } from '~/utils/crypto'
 import { signToken } from '~/utils/jwt'
 import { TokenType } from '~/constants/enums'
-
+import RefreshToken from '~/models/schemas/RefreshToken.schema'
+import { ObjectId } from 'mongodb'
+import { config } from 'dotenv'
+config()
 class UsersService {
   private signAccessToken(user_id: string) {
     return signToken({
@@ -28,6 +31,9 @@ class UsersService {
       }
     })
   }
+  private signAccessAndRefreshToken(user_id: string) {
+    return Promise.all([this.signAccessToken(user_id), this.signRefreshToken(user_id)])
+  }
   async register(payload: RegisterReqBody) {
     const { email, password } = payload
     const result = await databaseService.users.insertOne(
@@ -38,10 +44,10 @@ class UsersService {
       })
     )
     const user_id = result.insertedId.toString()
-    const [access_token, refresh_token] = await Promise.all([
-      this.signAccessToken(user_id),
-      this.signRefreshToken(user_id)
-    ])
+    const [access_token, refresh_token] = await this.signAccessAndRefreshToken(user_id)
+    await databaseService.refreshTokens.insertOne(
+      new RefreshToken({ user_id: new ObjectId(user_id), token: refresh_token })
+    )
     return {
       access_token,
       refresh_token
@@ -50,6 +56,16 @@ class UsersService {
   async checkEmailExist(email: string) {
     const user = await databaseService.users.findOne({ email })
     return Boolean(user)
+  }
+  async login(user_id: string) {
+    const [access_token, refresh_token] = await this.signAccessAndRefreshToken(user_id)
+    await databaseService.refreshTokens.insertOne(
+      new RefreshToken({ user_id: new ObjectId(user_id), token: refresh_token })
+    )
+    return {
+      access_token,
+      refresh_token
+    }
   }
 }
 
