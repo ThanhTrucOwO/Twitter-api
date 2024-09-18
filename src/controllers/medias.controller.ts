9a51c1180e0f02d00aa7eec13e151e62bd28e8e3
ThanhTrucOwO <kusodanma14@gmail.com>
2024-09-18 15:56:14 +0700

    Filter upload image

--- src/controllers/medias.controller.ts
+++ src/controllers/medias.controller.ts
@@ -1,20 +1,11 @@
 import { Request, Response, NextFunction } from 'express'
+import { result } from 'lodash'
 import path from 'path'
+import { handleUploadSingleImage } from '~/utils/file'
 
 export const uploadSingleImageController = async (req: Request, res: Response, next: NextFunction) => {
-  const formidable = (await import('formidable')).default
-  const form = formidable({
-    uploadDir: path.resolve('uploads'),
-    maxFiles: 1,
-    keepExtensions: true,
-    maxFileSize: 300 * 1024 // 300KB
-  })
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      throw err
-    }
-    res.json({
-      message: 'Upload image successfully'
-    })
+  const data = await handleUploadSingleImage(req)
+  return res.json({
+    result: data
   })
 }
