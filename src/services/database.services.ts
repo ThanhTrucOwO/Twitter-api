dae2d32e18438e9c740c6c5e9bcd54818e3b5e30
ThanhTrucOwO <kusodanma14@gmail.com>
2024-09-20 17:17:47 +0700

    Check status video encode

--- src/services/database.services.ts
+++ src/services/database.services.ts
@@ -3,6 +3,7 @@ import { config } from 'dotenv'
 import User from '~/models/schemas/User.schema'
 import RefreshToken from '~/models/schemas/RefreshToken.schema'
 import Follower from '~/models/schemas/Follower.schemas'
+import VideoStatus from '~/models/schemas/VideoStatus.schemas'
 config()
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@facebook.plk13.mongodb.net/?retryWrites=true&w=majority&appName=Facebook`
 
@@ -34,6 +35,9 @@ class DatabaseService {
 
   get followers(): Collection<Follower> {
     return this.db.collection(process.env.DB_FOLLOWERS_COLLECTION as string)
+  }
+  get videoStatus(): Collection<VideoStatus> {
+    return this.db.collection(process.env.DB_VIDEO_STATUS_COLLECTION as string)
   }
 }
 
