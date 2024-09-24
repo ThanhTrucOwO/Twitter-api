f6ee562fa4d6e24a16af7a92910ffc34b37ea509
ThanhTrucOwO <kusodanma14@gmail.com>
2024-09-24 14:37:19 +0700

    Pagination and increase view for new feeds

--- src/controllers/tweets.controller.ts
+++ src/controllers/tweets.controller.ts
@@ -60,6 +60,6 @@ export const getNewFeedsController = async (req: Request<ParamsDictionary, any, 
   const result = await tweetsService.getNewFeeds({ user_id, limit, page })
   return res.json({
     message: TWEETS_MESSAGES.GET_NEW_FEEDS_SUCCESSFULLY,
-    result
+    result: { tweets: result.tweets, limit, page, total_page: Math.ceil(result.total / limit) }
   })
 }
