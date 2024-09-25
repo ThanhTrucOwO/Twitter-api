fca274ac3a39eeef49624bc1d83c1003126388d5
ThanhTrucOwO <kusodanma14@gmail.com>
2024-09-25 15:06:18 +0700

    Send email when register successfully

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -14,6 +14,7 @@ import HTTP_STATUS from '~/constants/httpStatus'
 import Follower from '~/models/schemas/Follower.schemas'
 import axios from 'axios'
 import { verify } from 'crypto'
+import { sendVerifyEmail } from '~/utils/email'
 config()
 
 class UsersService {
@@ -113,7 +114,20 @@ class UsersService {
     await databaseService.refreshTokens.insertOne(
       new RefreshToken({ user_id: new ObjectId(user_id), token: refresh_token, iat, exp })
     )
-    console.log('email_verify_token: ', email_verify_token)
+    // Flow verify email
+    // 1. Server send email to user
+    // 2. User click link in email
+    // 3. Client send request to server with email_verify_token
+    // 4. Server verify email_verify_token
+    // 5. Client receive access_token and refresh_token
+    await sendVerifyEmail(
+      payload.email,
+      'Verify your email',
+      `
+      <h1>Verify your email</h1>
+      <p>Click <a href="${process.env.CLIENT_URL}/verify-email?token=${email_verify_token}">here</a> to verify your email</p>
+   `
+    )
     return {
       access_token,
       refresh_token
