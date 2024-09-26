14bbe88f0539b1b0d5ff85b16c0b5f6839fc6599
ThanhTrucOwO <kusodanma14@gmail.com>
2024-09-26 11:26:59 +0700

    Update Upload Image route to S3

--- src/services/medias.services.ts
+++ src/services/medias.services.ts
@@ -12,6 +12,8 @@ import { Media } from '~/models/Other'
 import { encodeHLSWithMultipleVideoStreams } from '~/utils/video'
 import databaseService from './database.services'
 import VideoStatus from '~/models/schemas/VideoStatus.schemas'
+import { uploadFileToS3 } from '~/utils/s3'
+import mime from 'mime'
 config()
 
 class Queue {
@@ -106,15 +108,26 @@ class MediasService {
     const result: Media[] = await Promise.all(
       files.map(async (file) => {
         const newName = getNameFromFullname(file.newFilename)
-        const newPath = path.resolve(UPLOAD_IMAGE_DIR, `${newName}.jpg`)
+        const newFullFilename = `${newName}.jpg`
+        const newPath = path.resolve(UPLOAD_IMAGE_DIR, newFullFilename)
+        sharp.cache(false) // fix EPERM: operation not permitted, unlink
         await sharp(file.filepath).jpeg().toFile(newPath)
-        fs.unlinkSync(file.filepath)
+        const s3Result = await uploadFileToS3({
+          filename: newFullFilename,
+          filepath: newPath,
+          contentType: mime.getType(newPath) as string
+        })
+        await Promise.all([fsPromise.unlink(file.filepath), fsPromise.unlink(newPath)])
         return {
-          url: isProduction
-            ? `${process.env.HOST}/static/image/${newName}.jpg`
-            : `http://localhost:${process.env.PORT}/static/image/${newName}.jpg`,
+          url: s3Result.Location as string,
           type: MediaType.Image
         }
+        // return {
+        //   url: isProduction
+        //     ? `${process.env.HOST}/static/image/${newFullFilename}`
+        //     : `http://localhost:${process.env.PORT}/static/image/${newFullFilename}`,
+        //   type: MediaType.Image
+        // }
       })
     )
     return result
--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -11,34 +11,49 @@ const s3 = new S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID as string
   }
 })
-const file = fs.readFileSync(path.resolve('uploads/images/641ef3576f45ce47816daa402.jpg'))
-const parallelUploads3 = new Upload({
-  client: s3,
-  params: { Bucket: 'twitternodejs-clone-2024', Key: 'anh-1.jpg', Body: file, ContentType: 'image/jpeg' },
+export const uploadFileToS3 = ({
+  filename,
+  filepath,
+  contentType
+}: {
+  filename: string
+  filepath: string
+  contentType: string
+}) => {
+  const parallelUploads3 = new Upload({
+    client: s3,
+    params: {
+      Bucket: 'twitternodejs-clone-2024',
+      Key: filename,
+      Body: fs.readFileSync(filepath),
+      ContentType: contentType
+    },
 
-  // optional tags
-  tags: [
-    /*...*/
-  ],
+    // optional tags
+    tags: [
+      /*...*/
+    ],
 
-  // additional optional fields show default values below:
+    // additional optional fields show default values below:
 
-  // (optional) concurrency configuration
-  queueSize: 4,
+    // (optional) concurrency configuration
+    queueSize: 4,
 
-  // (optional) size of each part, in bytes, at least 5MB
-  partSize: 1024 * 1024 * 5,
+    // (optional) size of each part, in bytes, at least 5MB
+    partSize: 1024 * 1024 * 5,
 
-  // (optional) when true, do not automatically call AbortMultipartUpload when
-  // a multipart upload fails to complete. You should then manually handle
-  // the leftover parts.
-  leavePartsOnError: false
-})
+    // (optional) when true, do not automatically call AbortMultipartUpload when
+    // a multipart upload fails to complete. You should then manually handle
+    // the leftover parts.
+    leavePartsOnError: false
+  })
+  return parallelUploads3.done()
+}
 
-parallelUploads3.on('httpUploadProgress', (progress) => {
-  console.log(progress)
-})
+// parallelUploads3.on('httpUploadProgress', (progress) => {
+//   console.log(progress)
+// })
 
-parallelUploads3.done().then((res) => {
-  console.log(res)
-})
+// parallelUploads3.done().then((res) => {
+//   console.log(res)
+// })
