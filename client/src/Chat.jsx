43a096d482d8dabfcf8d9ff20a83c74f3e419da9
ThanhTrucOwO <kusodanma14@gmail.com>
2024-09-26 22:29:29 +0700

    CSS for sender and receiver

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -12,33 +12,48 @@ export default function Chat() {
     socket.connect()
     socket.on('receive private message', (data) => {
       const content = data.content
-      setMessages((messages) => [...messages, content])
+      setMessages((messages) => [
+        ...messages,
+        {
+          content,
+          isSender: false
+        }
+      ])
     })
     return () => {
       socket.disconnect()
     }
   }, [])
 
-  const handleSubmit = (e) => {
+  const send = (e) => {
     e.preventDefault()
     setValue('')
     socket.emit('private message', {
       content: value,
       to: '66f4b65545a4f884a5fb53f7' // user_id của client 2
     })
+    setMessages((messages) => [
+      ...messages,
+      {
+        content: value,
+        isSender: true
+      }
+    ])
   }
 
   return (
     <div>
       <h1>Chat</h1>
-      <div>
+      <div className='chat'>
         {messages.map((message, index) => (
           <div key={index}>
-            <div>{message}</div>
+            <div className='message-container'>
+              <div className={'message ' + (message.isSender ? 'message-right' : '')}>{message.content}</div>
+            </div>
           </div>
         ))}
       </div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={send}>
         <input type='text' onChange={(e) => setValue(e.target.value)} value={value} />
         <button type='submit'>Send</button>
       </form>
