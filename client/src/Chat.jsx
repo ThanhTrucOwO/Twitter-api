55ddc4b7a915de4c82b0cc194c51dc6ffc30c00f
ThanhTrucOwO <kusodanma14@gmail.com>
2024-09-27 12:06:00 +0700

    Apply API get conversations vto client chat

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -15,7 +15,7 @@ export default function Chat() {
     }
   ]
   const [value, setValue] = useState('')
-  const [messages, setMessages] = useState([])
+  const [conversations, setConversations] = useState([])
   const [receiver, setReceiver] = useState('')
   const getProfile = (username) => {
     axios
@@ -32,34 +32,50 @@ export default function Chat() {
       _id: profile._id
     }
     socket.connect()
-    socket.on('receive private message', (data) => {
-      const content = data.content
-      setMessages((messages) => [
-        ...messages,
-        {
-          content,
-          isSender: false
-        }
-      ])
+    socket.on('receive_message', (data) => {
+      const { payload } = data
+      setConversations((conversations) => [...conversations, payload])
     })
     return () => {
       socket.disconnect()
     }
   }, [])
 
+  useEffect(() => {
+    if (receiver) {
+      axios
+        .get(`/conversations/receivers/${receiver}`, {
+          baseURL: import.meta.env.VITE_API_URL,
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('access_token')}`
+          },
+          params: {
+            limit: 10,
+            page: 1
+          }
+        })
+        .then((res) => {
+          setConversations(res.data.result.conversations)
+        })
+    }
+  }, [receiver])
+
   const send = (e) => {
     e.preventDefault()
     setValue('')
-    socket.emit('private message', {
+    const conversation = {
       content: value,
-      to: receiver,
-      from: profile._id
+      sender_id: profile._id,
+      receiver_id: receiver
+    }
+    socket.emit('send_message', {
+      payload: conversation
     })
-    setMessages((messages) => [
-      ...messages,
+    setConversations((conversations) => [
+      ...conversations,
       {
-        content: value,
-        isSender: true
+        ...conversation,
+        _id: new Date().getTime()
       }
     ])
   }
@@ -75,10 +91,12 @@ export default function Chat() {
         ))}
       </div>
       <div className='chat'>
-        {messages.map((message, index) => (
-          <div key={index}>
+        {conversations.map((conversation) => (
+          <div key={conversations._id}>
             <div className='message-container'>
-              <div className={'message ' + (message.isSender ? 'message-right' : '')}>{message.content}</div>
+              <div className={'message ' + (conversation.sender_id === profile._id ? 'message-right' : '')}>
+                {conversation.content}
+              </div>
             </div>
           </div>
         ))}
