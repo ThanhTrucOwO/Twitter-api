ca7d154f1e7a1b28455fbbd845e1e5801cdcd2c8
ThanhTrucOwO <kusodanma14@gmail.com>
2024-09-27 21:26:49 +0700

    Middleware Server Instance

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,12 @@ import { Server } from 'socket.io'
 import Conversation from './models/schemas/Conversations.schemas'
 import conversationsRouter from './routes/conversations.routes'
 import { ObjectId } from 'mongodb'
+import { verifyAccessToken } from './utils/commons'
+import { TokenPayload } from './models/requests/User.requests'
+import { UserVerifyStatus } from './constants/enums'
+import { ErrorWithStatus } from './models/Errors'
+import { USERS_MESSAGES } from './constants/messages'
+import HTTP_STATUS from './constants/httpStatus'
 
 config()
 databaseService.connect().then(() => {
@@ -58,9 +64,32 @@ const users: {
     socket_id: string
   }
 } = {}
+io.use(async (socket, next) => {
+  const { Authorization } = socket.handshake.auth
+  const access_token = Authorization?.split(' ')[1]
+  try {
+    const decoded_authorization = await verifyAccessToken(access_token)
+    const { verify } = decoded_authorization as TokenPayload
+    if (verify !== UserVerifyStatus.Verified) {
+      throw new ErrorWithStatus({
+        message: USERS_MESSAGES.EMAIL_NOT_VERIFIED,
+        status: HTTP_STATUS.FORBIDDEN
+      })
+    }
+    // Truyền decoded_authorization vào socket để sử dụng ở các middleware khác
+    socket.handshake.auth.decoded_authorization = decoded_authorization
+    next()
+  } catch (error) {
+    next({
+      message: 'Unauthorized',
+      name: 'UnauthorizedError',
+      data: error
+    })
+  }
+})
 io.on('connection', (socket) => {
   console.log(`User ${socket.id} connected`)
-  const user_id = socket.handshake.auth._id
+  const { user_id } = socket.handshake.auth.decoded_authorization as TokenPayload
   users[user_id] = {
     socket_id: socket.id
   }
@@ -68,9 +97,7 @@ io.on('connection', (socket) => {
   socket.on('send_message', async (data) => {
     const { receiver_id, sender_id, content } = data.payload
     const receiver_socket_id = users[receiver_id]?.socket_id
-    if (!receiver_socket_id) {
-      return
-    }
+
     const conversation = new Conversation({
       sender_id: new ObjectId(sender_id),
       receiver_id: new ObjectId(receiver_id),
@@ -78,9 +105,11 @@ io.on('connection', (socket) => {
     })
     const result = await databaseService.conversations.insertOne(conversation)
     conversation._id = result.insertedId
-    socket.to(receiver_socket_id).emit('receive_message', {
-      payload: conversation
-    })
+    if (receiver_socket_id) {
+      socket.to(receiver_socket_id).emit('receive_message', {
+        payload: conversation
+      })
+    }
   })
   socket.on('disconnect', () => {
     delete users[user_id]
